1fabea829e63d8b1572fad921850c4e56afc5e5b
danda <danda@osc.co.cr>
2018-07-18 13:30:21 +0800

    support scraping a single coin, and changing symbol of a coin, ie Bitgem BTG --> BTGEM because of collision with Bitcoin Gold BTG

--- tools/scrape-coinmarketcap-coins.php
+++ tools/scrape-coinmarketcap-coins.php
@@ -6,6 +6,7 @@ namespace coinparams\tools;
 require_once __DIR__  . '/../vendor/autoload.php';
 \strictmode\initializer::init();
 
+$argcoin = @$argv[1];
 
 /**
  * This tool scrapes metadata for all coins on coinmarketcap.com
@@ -16,6 +17,9 @@ $coinmeta = file_exists($coinfile) ? json_decode(file_get_contents($coinfile), t
 $coins = get_coin_list();
 
 foreach($coins as $info) {
+    if( $argcoin && $argcoin != $info['symbol']) {
+        continue;
+    }
     process_coin($coinmeta, $info);
     ksort($coinmeta);
     file_put_contents($coinfile, json_encode($coinmeta, JSON_PRETTY_PRINT));
@@ -26,6 +30,15 @@ echo "Data written to $coinfile\n";
 function process_coin(&$meta, $info) {
     $symbol = $info['symbol'];
     $name = $info['name'];
+    $symbol_comment = null;
+    
+    $resymbol_map = [
+        'Bitgem' => ['BTGEM', 'BTG --> BTGEM due to collision between Bitcoin Gold and Bitgem.  Bitcoin Gold presently has much higher market cap and slip44 entry, so it won.'],
+    ];
+    if( @$resymbol_map[$name]) {
+        list($symbol, $symbol_comment) = $resymbol_map[$name];
+    }
+    
     echo "\nProcessing $symbol - $name\n";
     
     if( @$meta[$symbol] ) {
@@ -38,6 +51,9 @@ function process_coin(&$meta, $info) {
     $buf = file_get_contents($url);
     
     $d = ['symbol' => $symbol, 'name' => $name ];
+    if($symbol_comment) {
+        $d['symbol_comment'] = $symbol_comment;
+    }
 
     $rc = preg_match_all('#<span class="label label-warning">(.*)</span>#', $buf, $matches);
     $tags = $rc ? $matches[1] : [];
